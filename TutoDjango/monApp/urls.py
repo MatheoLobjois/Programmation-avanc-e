1167e8d9fed961a781b801113af06436d26e1863
MatheoLobjois <matheo.lobjois@etu.univ-orleans.fr>
2025-09-02 18:30:42 +0200

    Fin du TD1

---
+++ TutoDjango/monApp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+ path("home/<param>", views.home, name="home"),
+ path("AboutUs", views.about_us, name="about_us"),
+ path("ContactUs", views.contact_us, name="contact_us"),
+]
---
+++ TutoDjango/monApp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def home(request ,param):
+    return HttpResponse(f"<h1>Hello {param} </h1>")
+
+
+def about_us(request):
+    return HttpResponse("<h1> About us </h1>")
+
+def contact_us(request):
+    return HttpResponse("<h1> ? </h1>")
