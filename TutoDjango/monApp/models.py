4d3e6b0536001f9c057582a7b9ee5a6478bb0333
MatheoLobjois <matheo.lobjois@etu.univ-orleans.fr>
2025-09-11 09:19:56 +0200

    Correction merge Avancée TD2 et Fin TD2

--- TutoDjango/monApp/models.py
+++ TutoDjango/monApp/models.py
@@ -20,11 +20,11 @@ class Produit(models.Model):
     refProd = models.AutoField(primary_key=True)
     intituleProd = models.CharField(max_length=200)
     prixUnitaireProd = models.DecimalField(max_digits=10, decimal_places=2)
-    dateFabProd = models.DateField(default=date.today)
+    dateFabrication = models.DateField(default=date.today)
 
     # Relation CIF : chaque produit appartient à 1 catégorie (0,N côté catégorie → 1,1 côté produit)
     categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE, related_name="produits_categorie", null=True, blank=True)
-    status = models.ForeignKey(Statut, on_delete=models.CASCADE, related_name="produits_status",null=True, blank=True)
+    statut = models.ForeignKey(Statut, on_delete=models.CASCADE, related_name="produits_status",null=True, blank=True)
 
     def __str__(self):
         return self.intituleProd
